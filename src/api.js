6e0e394f769e7ad220696329419d7e7d17b5f0fb
Alfredo Figueroa <alfredojfigueroa@gmail.com>
2022-08-17 21:24:59 -0400

    pushing new react app to github.

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ba329c945amsh1ee6f5624992713p194754jsn2e2ab53c8273',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+
+export const WEATHER_API_KEY = '5b48fc2eca219b4e2105feef435e035c'
